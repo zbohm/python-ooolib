eda8f86aa9773d1b06c12031a58a39c7b19a97ea
Zdeněk Böhm <zdenek.bohm@nic.cz>
2019-05-09 16:04:18 +0200

    Use setuptools instead of distutils in setup.

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from distutils.core import setup
-import distutils.core
+import setuptools
 import ooolib
 
-setup(name="ooolib-python",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+      name="ooolib-python",
       version=ooolib.version_number(),
       description="Package for creating OpenDocument Format(ODF) spreadsheets.",
       author="Joseph Colton",
       author_email="joseph@colton.byuh.edu",
       maintainer=u"Zdeněk Böhm",
       maintainer_email="zdenek.bohm@nic.cz",
-      license = 'GNU LGPL',
-      url = "https://github.com/zbohm/python-ooolib",
-      packages=['ooolib', 'ooolib.tests']
-      )
+      license='GNU LGPL',
+      url="https://github.com/zbohm/python-ooolib",
+      long_description=long_description,
+      packages=['ooolib', 'ooolib.tests'],
+      classifiers=[
+         "Programming Language :: Python :: 2.7",
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+         "Operating System :: OS Independent",
+      ]
+)
