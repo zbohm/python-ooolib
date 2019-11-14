04454049f52275324146f210ff829a086034c9c9
Zdeněk Böhm <zdenek.bohm@nic.cz>
2019-11-14 16:57:08 +0100

    Add fixtures test-cells.ods.

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import setuptools
+from setuptools import find_packages, setup
 import ooolib
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
-      name="ooolib-python",
-      version=ooolib.version_number(),
-      description="Package for creating OpenDocument Format(ODF) spreadsheets.",
-      author="Joseph Colton",
-      author_email="joseph@colton.byuh.edu",
-      maintainer=u"Zdeněk Böhm",
-      maintainer_email="zdenek.bohm@nic.cz",
-      license='MIT',
-      url="https://github.com/zbohm/python-ooolib",
-      long_description=long_description,
-      packages=['ooolib', 'ooolib.tests'],
-      classifiers=[
-         "Development Status :: 5 - Production/Stable",
-         "Programming Language :: Python :: 2.7",
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-      ]
+setup(
+    name="ooolib-python",
+    version=ooolib.version_number(),
+    description="Package for creating OpenDocument Format(ODF) spreadsheets.",
+    author="Joseph Colton",
+    author_email="joseph@colton.byuh.edu",
+    maintainer=u"Zdeněk Böhm",
+    maintainer_email="zdenek.bohm@nic.cz",
+    license='MIT',
+    url="https://github.com/zbohm/python-ooolib",
+    long_description=long_description,
+    packages=find_packages(),
+    package_data={
+        "ooolib": ["tests/fixtures/test-cells.ods"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
