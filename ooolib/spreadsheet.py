ac405f1eb4977bd37d445b8ce13d68b92643ef49
Zdeněk Böhm <zdenek.bohm@nic.cz>
2024-10-16 15:44:37 +0200

    Update F10.ods.

--- ooolib/spreadsheet.py
+++ ooolib/spreadsheet.py
@@ -107,14 +107,14 @@ class Spreadsheet(RootMixin):
         <table:table-column table:style-name="co1" table:default-cell-style-name="Default"/>
         <table:table-column table:style-name="co2" table:number-columns-repeated="5" table:default-cell-style-name="Default"/>
 
-1        <table:table-row table:style-name="ro1">
+1       <table:table-row table:style-name="ro1">
           <table:table-cell office:value-type="string" calcext:value-type="string">
             <text:p>A1</text:p>
           </table:table-cell>
           <table:table-cell table:number-columns-repeated="5"/>
         </table:table-row>
 
-2        <table:table-row table:style-name="ro1">
+2       <table:table-row table:style-name="ro1">
           <table:table-cell/>
           <table:table-cell office:value-type="float" office:value="22" calcext:value-type="float">
             <text:p>22</text:p>
@@ -122,32 +122,38 @@ class Spreadsheet(RootMixin):
           <table:table-cell table:number-columns-repeated="4"/>
         </table:table-row>
 
-3        <table:table-row table:style-name="ro1">
+3       <table:table-row table:style-name="ro1">
           <table:table-cell/>
           <table:table-cell table:formula="of:=SUM([.B2:.B2])" office:value-type="float" office:value="22" calcext:value-type="float">
             <office:annotation draw:style-name="gr1" draw:text-style-name="P2" svg:width="2.899cm" svg:height="1.799cm" svg:x="6.283cm" svg:y="0cm" draw:caption-point-x="-0.61cm" draw:caption-point-y="0.913cm">
               <dc:date>2024-10-16T00:00:00</dc:date>
-              <text:p text:style-name="P1">Summary.</text:p>
+              <text:p text:style-name="P1">
+                <text:span text:style-name="T1">Summary.</text:span>
+              </text:p>
             </office:annotation>
             <text:p>22</text:p>
           </table:table-cell>
           <table:table-cell table:number-columns-repeated="4"/>
         </table:table-row>
 
-4        <table:table-row table:style-name="ro1">
+4       <table:table-row table:style-name="ro1">
           <table:table-cell/>
           <table:table-cell>
             <office:annotation draw:style-name="gr1" draw:text-style-name="P2" svg:width="2.899cm" svg:height="1.799cm" svg:x="6.283cm" svg:y="0cm" draw:caption-point-x="-0.61cm" draw:caption-point-y="1.364cm">
               <dc:date>2024-10-16T00:00:00</dc:date>
-              <text:p text:style-name="P1">Empty.</text:p>
+              <text:p text:style-name="P1">
+                <text:span text:style-name="T1">Empty.</text:span>
+              </text:p>
             </office:annotation>
           </table:table-cell>
           <table:table-cell table:number-columns-repeated="4"/>
         </table:table-row>
 
-5        <table:table-row table:style-name="ro1">
+5       <table:table-row table:style-name="ro1">
           <table:table-cell table:number-columns-repeated="2"/>
-          <table:table-cell table:number-columns-spanned="2" table:number-rows-spanned="1"/>
+          <table:table-cell office:value-type="string" calcext:value-type="string" table:number-columns-spanned="2" table:number-rows-spanned="1">
+            <text:p>TU</text:p>
+          </table:table-cell>
           <table:covered-table-cell/>
           <table:table-cell office:value-type="string" calcext:value-type="string">
             <text:p>E5</text:p>
@@ -155,29 +161,31 @@ class Spreadsheet(RootMixin):
           <table:table-cell/>
         </table:table-row>
 
-6        <table:table-row table:style-name="ro1">
+6       <table:table-row table:style-name="ro1">
           <table:table-cell table:number-columns-repeated="6"/>
         </table:table-row>
 
-7        <table:table-row table:style-name="ro1">
+7       <table:table-row table:style-name="ro1">
           <table:table-cell table:number-columns-repeated="2"/>
-          <table:table-cell table:number-columns-spanned="1" table:number-rows-spanned="3"/>
+          <table:table-cell office:value-type="string" calcext:value-type="string" table:number-columns-spanned="1" table:number-rows-spanned="3">
+            <text:p>SOM</text:p>
+          </table:table-cell>
           <table:table-cell table:number-columns-repeated="3"/>
         </table:table-row>
 
-8        <table:table-row table:style-name="ro1">
-          <table:table-cell table:number-columns-repeated="2"/>
-          <table:covered-table-cell/>
-          <table:table-cell table:number-columns-repeated="3"/>
-        </table:table-row>
-
-9        <table:table-row table:style-name="ro1">
+8       <table:table-row table:style-name="ro1">
           <table:table-cell table:number-columns-repeated="2"/>
           <table:covered-table-cell/>
           <table:table-cell table:number-columns-repeated="3"/>
         </table:table-row>
 
-10        <table:table-row table:style-name="ro1">
+9       <table:table-row table:style-name="ro1">
+          <table:table-cell table:number-columns-repeated="2"/>
+          <table:covered-table-cell/>
+          <table:table-cell table:number-columns-repeated="3"/>
+        </table:table-row>
+
+10     <table:table-row table:style-name="ro1">
           <table:table-cell office:value-type="string" calcext:value-type="string">
             <text:p>
               <text:a xlink:href="https://example.com/" xlink:type="simple">https://example.com</text:a>
@@ -188,6 +196,7 @@ class Spreadsheet(RootMixin):
             <text:p>F10</text:p>
           </table:table-cell>
         </table:table-row>
+
       </table:table>
       <table:named-expressions/>
     </office:spreadsheet>
